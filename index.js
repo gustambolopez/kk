da9ef71d01b1c0aa3379df038f8c581576891e07
gustambolopez <mogolixzq@gmail.com>
2025-07-18 21:41:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 app.use(cookieParser());
 
 const customProxy = createProxyMiddleware({
-  target: "https://arsenic.smartfoloo.space/",
+  target: "https://yandex.com/games",
   changeOrigin: true,
   onProxyReq: (proxyReq) => {
     storedCookies.forEach((cookie) => {
